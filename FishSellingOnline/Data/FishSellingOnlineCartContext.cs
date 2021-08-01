aa6c44bd95a8b03d38dabaeff96e962d983fedf6
SAMUEL ANDREW <sauperman21@gmail.com>
2021-08-01 21:04:25 +0800

    Added cart

---
+++ FishSellingOnline/Data/FishSellingOnlineCartContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FishSellingOnline.Models;
+
+namespace FishSellingOnline.Data
+{
+    public class FishSellingOnlineCartContext : DbContext
+    {
+        public FishSellingOnlineCartContext(DbContextOptions<FishSellingOnlineCartContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<FishSellingOnline.Models.Cart> Cart { get; set; }
+    }
+}
---
+++ FishSellingOnline/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishSellingOnline.Models
+{
+    public class Cart
+    {
+        public int CartID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public byte[] ProductImage { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public int Total { get; set; }
+    }
+}
