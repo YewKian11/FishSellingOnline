a82f5b7162773cf2807554f8ab70b17debf30402
KONG YEW KIAN <TP058092@mail.apu.edu.my>
2021-08-01 22:31:45 +0800

    message

--- FishSellingOnline/Controllers/BlobsController.cs
+++ FishSellingOnline/Controllers/BlobsController.cs
@@ -37,5 +37,90 @@ namespace FishSellingOnline.Controllers
 
             return View();
         }
+        // Gallery 
+        public ActionResult ListItemGallery(string Message = null) {
+
+            ViewBag.msg = Message;
+            //get container information
+            CloudBlobContainer container = getBlobContainerInformation();
+
+            // create list, store blobs information
+            List<string> blobs = new List<string>();
+
+            //Find Blob Result
+            BlobResultSegment result = container.ListBlobsSegmentedAsync(null).Result;
+
+            //track blob
+            foreach (IListBlobItem item in result.Results) 
+            {
+                //blob type identify 
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    if (Path.GetExtension(blob.Name.ToString()) == ".jpg" ||
+                        Path.GetExtension(blob.Name.ToString()) == ".png") { 
+                    blobs.Add(blob.Name + "#" + blob.Uri.ToString());
+                    }
+                    
+                } 
+                else if(item.GetType() ==typeof(CloudPageBlob)) 
+                {
+                    CloudPageBlob blob = (CloudPageBlob)item;
+                    blobs.Add(blob.Name + "#" + blob.Uri.ToString());
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory blob = (CloudBlobDirectory)item;
+                    blobs.Add(blob.Uri.ToString());
+                }
+            }
+            return View(blobs);
+        }
+
+        //Download Blob refer, asp-action 
+        public ActionResult downloadblob(string imagename,string urlimage) {
+            CloudBlobContainer container = getBlobContainerInformation();
+            string message = null;
+
+            try
+            {
+                CloudBlockBlob item = container.GetBlockBlobReference(imagename);
+                var outputitem = System.IO.File.OpenWrite(@"C:\Users\Public\Pictures\" + imagename);
+                item.DownloadToStreamAsync(outputitem).Wait();
+                message = imagename + " is downloaded successfully";
+                outputitem.Close();
+            }
+            catch (Exception ex)
+            {
+                message = "Unable to download file " + imagename +
+                        "\\n Technical issues:" + ex.ToString() + "Please try dowload file again";
+            }
+            return RedirectToAction("ListItemGallery", "Blobs", new { Message = message });
+        }
+
+
+
+        //Delete Blob refer, asp-action
+        public ActionResult deleteblob(string area)
+        {
+            CloudBlobContainer container = getBlobContainerInformation();
+            string message = "";
+
+
+            try 
+            {
+                CloudBlockBlob item = container.GetBlockBlobReference(area);
+                message = item.Name + " is deleted";
+                item.DeleteIfExistsAsync();
+            }
+            catch (Exception ex)
+            {
+                message = "Unable to download file " + area +
+                        "\\n Technical issues:" + ex.ToString() + "Please try dowload file again";
+            }
+
+            return RedirectToAction("ListItemGallery", "Blobs", new { Message = message});
+
+        }
     }
 }
